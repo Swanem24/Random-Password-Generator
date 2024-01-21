77237ac69f59e1f684896097e4d8539d6b7825b3
Sanjog Wanem <37484722+Swanem24@users.noreply.github.com>
2024-01-21 15:28:55 +0000

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("password"); 
+
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#!£$%^&*(){}\/";
+
+async function createPassword() {
+    let password = "";
+
+    while(password.length < length) {
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += number[Math.floor(Math.random() * number.length)];
+        password += symbol[Math.floor(Math.random() * symbol.length)];
+    }
+
+    passwordBox.value = password;
+}
